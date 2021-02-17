619d92b816284ca9f049815cd4f8d1c42ad658fe
Art Tucker <art.w.tucker@gmail.com>
2021-02-17 03:20:14 -0800

    update buildCharts function

--- static/js/charts.js
+++ static/js/charts.js
@@ -55,35 +55,38 @@ function buildMetadata(sample) {
 }
 
 // 1. Create the buildCharts function.
-//function buildCharts(sample) {
+function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  //d3.json("samples.json").then((data) => {
+  d3.json(samples_json).then((data) => {
     // 3. Create a variable that holds the samples array. 
-  
-
+    var samplesArr = data.samples;
+    //console.log(samplesArr);
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var resultA = samplesArr.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-
-
+    var result = resultA[0];
+    console.log(result);
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otuIDs = result.otu_ids;
+    var otuLabels = result.otu_labels;
+    var sampleValues = result.sample_values;
+    console.log(sampleValues);
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
+    // so the otu_ids with the most bacteria are last. 
 
-    //var yticks = [];
+    var yticks = [];
 
     // 8. Create the trace for the bar chart. 
-    //var barData = [
+    var barData = [
       
-    //];
+    ];
     // 9. Create the layout for the bar chart. 
-    //var barLayout = {
+    var barLayout = {
      
-    //};
+    };
     // 10. Use Plotly to plot the data with the layout. 
     
-  //});
-//}
+  });
+}
