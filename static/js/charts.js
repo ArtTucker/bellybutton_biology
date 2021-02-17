a7aafa750ab9d6d249a770d2f6f4a3943d2444d0
Art Tucker <art.w.tucker@gmail.com>
2021-02-17 04:58:19 -0800

    add starter code for bubble chart

--- static/js/charts.js
+++ static/js/charts.js
@@ -101,5 +101,19 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot('bar', barData, barLayout);
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+   
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    
+
   });
 }
