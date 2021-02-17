81d5cd1f22f4bfb42221157bdc4c4532f4a9302c
Art Tucker <art.w.tucker@gmail.com>
2021-02-16 23:59:16 -0800

    set up var for samples.json

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,9 +1,11 @@
+var samples_json = 'static/json/samples.json'
+
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("static/json/samples.json").then((data) => {
+  d3.json(samples_json).then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -31,7 +33,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("static/json/samples.json").then((data) => {
+  d3.json(samples_json).then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -53,10 +55,11 @@ function buildMetadata(sample) {
 }
 
 // 1. Create the buildCharts function.
-function buildCharts(sample) {
+//function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("static/json/samples.json").then((data) => {
+  //d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
+  
 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
 
@@ -70,17 +73,17 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    //var yticks = [];
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
+    //var barData = [
       
-    ];
+    //];
     // 9. Create the layout for the bar chart. 
-    var barLayout = {
+    //var barLayout = {
      
-    };
+    //};
     // 10. Use Plotly to plot the data with the layout. 
     
-  });
-}
+  //});
+//}
